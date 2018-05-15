1d545e5685715ea2d8fbd5bea105509ff7a65fea
Denis Denisenko <d.winmain@gmail.com>
2018-05-15 10:35:28 +0700

    Version 0.5.6.1: Date fix

--- webby-haxe/src/js/form/field/RuDateField.hx
+++ webby-haxe/src/js/form/field/RuDateField.hx
@@ -24,7 +24,7 @@ class RuDateField extends AbstractDateField {
         a.push(v);
       }
       if (a[2] > 1900) {
-        var d = new Date(a[2], a[1] - 1, a[0]);
+        var d = new Date(a[2], a[1] - 1, a[0], 0, 0, 0);
         if (G.toInt(a[0]) == d.getDate() && G.toInt(a[1]) == (d.getMonth() + 1) && G.toInt(a[2]) == d.getFullYear()) {
           return d;
         }
